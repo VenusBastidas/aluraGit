1f909fb9c6a64f6da4e3fa1edd3d926ef09f6224
Venus <venus.bastidas.g@gmail.com>
2022-08-24 00:03:58 -0400

    New msg

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function jump(){
+
+    document.write('<br>');
+    document.write('<br>');
+    document.write('<br>');
+};
+
+function print(algo){
+    document.write(algo);
+    jump();
+};
+
+
